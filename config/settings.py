60adc4dcf05985dc839b1a46c06f714f946b8e2f
shutyforce <bbshutyforcebn@gmail.com>
2020-06-08 21:15:20 +0900

    hello

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +30,7 @@ SECRET_KEY = 'ylf09=-$*b9gmrif0*!61j38qc+@8pi_486ob7%q205ih#3*3r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # EMAIL_BACKEND = 'smtp.gmail.com
 EMAIL_HOST_USER = "komkomdjango21@gmail.com"
 EMAIL_HOST = 'smtp.gmail.com'
@@ -126,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
